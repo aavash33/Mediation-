cadf7c029a8697f44c7989941434a1d79fee7dc4
aavash33 <131329762+aavash33@users.noreply.github.com>
2025-05-08 18:27:09 +1000

    Delete Mediation directory

--- Mediation/Mediation/vendor/autoload.php
+++
@@ -1,4 +0,0 @@
-<?php
-require_once __DIR__ . '/phpmailer/PHPMailer/src/PHPMailer.php';
-require_once __DIR__ . '/phpmailer/PHPMailer/src/SMTP.php';
-require_once __DIR__ . '/phpmailer/PHPMailer/src/Exception.php';
